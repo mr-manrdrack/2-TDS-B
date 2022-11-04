9a90a4e25d8ff1046f7fa4773c02dd517a0627ba
mr-manrdrack <117283984+mr-manrdrack@users.noreply.github.com>
2022-11-04 19:29:21 -0300

    Add files via upload

---
+++ HTML - CSS/LPR/Java/script.js
@@ -0,0 +1,16 @@
+function coletar(){
+    var alter = document.getElementById("texto").value;
+    alert(alter);
+    console.log(java)
+}
+
+function excluir(){
+    const ex = document.getElementById("texto").value=" ";
+}
+
+function printardiv(){
+    var print = document.getElementById("texto").value;
+    var div = document.getElementById("exibo");
+
+    div.innerHTML = print
+}
